f413550d115a608452e1e9e6b2420ac2c4988f95
qiaoyu <qiaoyu1111358@gmail.com>
2017-07-18 20:38:03 +0800

    implement cloud#create

--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -2,4 +2,23 @@ class CloudsController < ApplicationController
   def index
     @clouds = Cloud.all
   end
+
+  def show
+  end
+
+  def new
+    @cloud = Cloud.new
+  end
+
+  def create
+    @cloud = Cloud.new(cloud_params)
+    @cloud.save
+    redirect_to clouds_path
+  end
+
+  private
+
+  def cloud_params
+    params.require(:cloud).permit(:title, :body)
+  end
 end
